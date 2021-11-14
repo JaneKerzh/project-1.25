40bf92596674f013e9db273ea97955404724254c
JaneKerzh <ekerzhentseva@rocketsoftware.com>
2021-11-14 14:45:40 +0100

    listener added dynamically

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ function setImagesZindex() {
 
 
 };
+
+window.addEventListener("DOMContentLoaded", function() {
+  var menu_buttons = document.getElementsByClassName('topnav_button');
+
+  console.log("length = " + menu_buttons.length);
+
+  for (var i = 0; i < menu_buttons.length; i++) {
+      menu_buttons[i].addEventListener('click', setImagesZindex, false);
+      console.log('listener added')
+  }
+
+}, false);
