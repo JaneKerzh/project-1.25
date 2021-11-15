dd17ae0fea92457ae3391a4a87c4306c21da4ff6
JaneKerzh <ekerzhentseva@rocketsoftware.com>
2021-11-15 14:16:27 +0100

    sclicking behaviour changed

--- script.js
+++ script.js
@@ -3,26 +3,19 @@ function setImagesZindex() {
     var style = window.getComputedStyle(element);
     var current_z = style.getPropertyValue('z-index');
 
-    if (current_z > 0)
-      {
-        document.getElementById("images").style.zIndex = "-1";
-            console.log("now it's -1");
-      } else {
-          document.getElementById("images").style.zIndex = "1";
-            console.log("now it's -1");
-      }
-
+    console.log(event.target.outerText)
+    if (event.target.outerText == "invest") {
+      document.getElementById("images").style.zIndex = "-1";
+    } else {
+      document.getElementById("images").style.zIndex = "1";
+    }
 
 };
 
 window.addEventListener("DOMContentLoaded", function() {
   var menu_buttons = document.getElementsByClassName('topnav_button');
-
-  console.log("length = " + menu_buttons.length);
-
   for (var i = 0; i < menu_buttons.length; i++) {
       menu_buttons[i].addEventListener('click', setImagesZindex, false);
-      console.log('listener added')
   }
 
 }, false);
